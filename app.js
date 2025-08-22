22e454a1fb45a47667924117f02845e22a8319b3
kerthiks <kerthik.cse2019gcedpi.edu.in>
2025-08-22 08:30:34 +0530

    blue_green

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-const http = require('http');
-process.env.ENVIRONMENT = 'production';3000;
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});d(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from ' + process.env.ENVIRONMENT + ' environment!\n');    res.end('Hello from ' + process.env.ENVIRONMENT + ' environment!\n');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello from ' + process.env.ENVIRONMENT + ' environment!\n');
 }).listen(port);
 
-
-
-
-console.log('Server running on port', port);console.log('Server running on port', port);
+console.log('Server running on port', port);
